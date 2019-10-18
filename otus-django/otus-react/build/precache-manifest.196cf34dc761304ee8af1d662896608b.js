d6f39e072db78365b1abd3d2761e7c5b73380cc6
ravique <andres@space-coding.ru>
2019-10-18 20:06:57 +0300

    Initial

---
+++ otus-django/otus-react/build/precache-manifest.196cf34dc761304ee8af1d662896608b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6b29a3027dad65f282431e03d8a92f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ca917e61d8c65fe678",
+    "url": "/static/css/2.b03de2b6.chunk.css"
+  },
+  {
+    "revision": "e5e183f47ee5fb1d7d11",
+    "url": "/static/css/main.369abf0e.chunk.css"
+  },
+  {
+    "revision": "89ca917e61d8c65fe678",
+    "url": "/static/js/2.4147ac0a.chunk.js"
+  },
+  {
+    "revision": "e5e183f47ee5fb1d7d11",
+    "url": "/static/js/main.e08009ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
